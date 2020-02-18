097de569fa82c04cefdbb7c5b6eb522266b1c2fd
cm8 <cm8@users.noreply.github.com>
2020-02-18 04:22:46 +0100

    stick to tested git rev of zimhttpserver unless git latest is chosen

--- make/zimhttpserver/zimhttpserver.mk
+++ make/zimhttpserver/zimhttpserver.mk
@@ -1,8 +1,17 @@
-$(call PKG_INIT_BIN, endian)
-$(PKG)_BINARY:=$($(PKG)_DIR)/zimHttpServer.pl
-$(PKG)_SITE:=git@https://github.com/waynepiekarski/zimHttpServer32
-$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+ZIMHTTPSERVER_GIT_REPOSITORY := https://github.com/waynepiekarski/zimHttpServer32
+$(call PKG_INIT_BIN, $(if $(FREETZ_PACKAGE_ZIMHTTPSERVER_VERSION_LATEST_GIT),$(call \
+	git-get-latest-revision,$(ZIMHTTPSERVER_GIT_REPOSITORY),endian),bcac442183))
+
+$(PKG)_BINARY := $($(PKG)_DIR)/zimHttpServer.pl
+$(PKG)_SITE := git@$($(PKG)_GIT_REPOSITORY)
+$(PKG)_SOURCE := $(pkg)-$($(PKG)_VERSION).tar.xz
+$(PKG)_TARGET_BINARY := $($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+
+$(PKG)_BUILD_PREREQ += git
+$(PKG)_BUILD_PREREQ_HINT := Hint: on debian-like systems "sudo apt-get install git"
+
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_ZIMHTTPSERVER_VERSION_LATEST_TESTED
+$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_ZIMHTTPSERVER_VERSION_LATEST_GIT
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
